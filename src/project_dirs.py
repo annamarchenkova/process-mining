964ae51ea289ca000f9f2692f2071f66d697ff7f
Anna Marchenkova <45556524+annamarchenkova@users.noreply.github.com>
2023-12-22 12:34:35 +0100

    Create project_dirs.py

---
+++ src/project_dirs.py
@@ -0,0 +1,7 @@
+import os
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parents[1]
+DATA_DIR = os.path.join(PROJECT_DIR, 'data')
+OUTPUT_DIR = os.path.join(PROJECT_DIR, 'output')
+FIGURES_DIR = os.path.join(OUTPUT_DIR, 'figures')
